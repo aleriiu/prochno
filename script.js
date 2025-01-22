0f2db48b99a61942be60864b95d8e02fa7dc3545
aleriiu <aleriiiu@gmail.com>
2025-01-22 17:34:32 +0300

    added

--- script.js
+++ script.js
@@ -2,20 +2,20 @@
 
 //swiper sliders
 const swiper1thumbs = new Swiper(".mySwiper-1-thumbs", {
-    spaceBetween: 16,
-    slidesPerView: 'auto',
-    freeMode: true,
-    watchSlidesProgress: true,
-  });
+  spaceBetween: 16,
+  slidesPerView: 'auto',
+  freeMode: true,
+  watchSlidesProgress: true,
+});
 
 const swiper1 = new Swiper(".mySwiper-1", {
-    spaceBetween: 0,
-    slidesPerView: 'auto',
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    thumbs: {
-      swiper: swiper1thumbs,
-    },
-  });
+  spaceBetween: 0,
+  slidesPerView: 'auto',
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper1thumbs,
+  },
+});
