67f8e969c3d8d966d01cad0377cc7b35c85686b0
aleriiu <aleriiiu@gmail.com>
2025-01-23 20:16:00 +0300

    added form.js

---
+++ form.js
@@ -0,0 +1,12 @@
+'use strict';
+
+
+function checkCheckbox() {
+    const checkbox = document.getElementById('myCheckbox');
+    if (checkbox.checked) {
+      console.log('Checkbox is checked');
+    } else {
+      console.log('Checkbox is not checked');
+    }
+  }
+
