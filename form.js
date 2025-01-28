5cfbf67a12249191bcd17546041c95ba2f102fbf
pirogue.one <alyona.evg@yandex.ru>
2025-01-28 19:54:43 +0300

    form handle and small changes

--- form.js
+++ form.js
@@ -1,12 +1,22 @@
 'use strict';
 
+function handleFormSubmit(event) {
+  // Просим форму не отправлять данные самостоятельно
+  event.preventDefault()
+  formBtn.style.backgroundColor = "#FFF";
+  formBtn.innerText = "Отправлено!";
+  formBtn.style.color = "#028164";
+  nameInput.value = '';
+  phoneInput.value = '';
+  formBtn.disabled = true;
+  formBtn.style.cursor = "auto";
+}
 
-function checkCheckbox() {
-    const checkbox = document.getElementById('myCheckbox');
-    if (checkbox.checked) {
-      console.log('Checkbox is checked');
-    } else {
-      console.log('Checkbox is not checked');
-    }
-  }
+const applicantForm = document.querySelector('.form');
+const formBtn = applicantForm.querySelector('button');
+const nameInput = applicantForm.querySelector('.input-name');
+const phoneInput = applicantForm.querySelector('.input-phone');
+
+applicantForm.addEventListener('submit', handleFormSubmit);
+
 
